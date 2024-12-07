5a0830808f4de9c74eb0ae806ee6a898564cae57
angelina43 <bushueva.03@inbox.ru>
2024-12-07 13:04:21 +0700

    middleware для Админа работает

--- routes/api.php
+++ routes/api.php
@@ -8,24 +8,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //админка
-Route::controller(AdminCotroller::class)->group(function() {
-    Route::post('admin/users/create', 'create')->middleware('auth:sanctum', 'admin'); //создание пользователя
-    Route::get('admin/users/index', 'index')->middleware('auth:sanctum', 'admin'); //просмотр всех пользователей
-    Route::delete('admin/users/delete', 'deleteUser')->middleware('auth:sanctum', 'admin'); //удаление пользователя
-    Route::delete('admin/posts/delete', 'deletePost')->middleware('auth:sanctum', 'admin'); //удаление поста
-    Route::delete('admin/comment/delete', 'deleteComment')->middleware('auth:sanctum', 'admin'); //удаление комментария
+Route::controller(AdminCotroller::class)->group(function () {
+    Route::group(['middleware' => ['auth:sanctum', 'admin']], function () {
+        Route::get('admin/users/index', 'index'); //просмотр всех пользователей
+        Route::delete('admin/users/delete', 'deleteUser'); //удаление пользователя
+        Route::delete('admin/posts/delete', 'deletePost'); //удаление поста
+        Route::delete('admin/comment/delete', 'deleteComment'); //удаление комментария
+    });
 });
 
 //неавторизованный пользователь
 Route::get('posts/index', [PostController::class, 'index']); //главная страница со всеми постами
 
-Route::controller(RegisterController::class)->group(function() {
+Route::controller(RegisterController::class)->group(function () {
     Route::post('register', 'register'); //регистрация
     Route::post('login', 'login'); //авторизация
 });
 
 //авторизованный пользователь
-Route::middleware('auth:sanctum')->group( function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [RegisterController::class, 'logout']); //выход
     Route::get('profile', [ProfileController::class, 'index']); //профиль
     Route::post('profile/avatar', [ProfileController::class, 'avatar']); //смена автара
